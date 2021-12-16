f6ea725f3714f1b4483656926fe980b5a47a7cd4
tiepcodegym <viettiep122@gmail.com>
2021-12-16 19:51:22 +0700

    hello

---
+++ baiTH1/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "<h2>This is Home page</h2>";
+});
+
+Route::get('/about', function () {
+    echo "<h2>This is About page</h2>";
+});
+
+Route::get('/contact', function () {
+    echo "<h2>This is Contact page</h2>";
+});
+Route::get('/user', function () {
+    return view('user', ['name'=>'Masud Alam']);
+});
+Route::get('/', 'App\Http\Controllers\HomeController@index');
